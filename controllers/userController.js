927a76925f28480c960eec5105b687eb8a08ca13
Smit Koli <smit.koli@morningstar.com>
2021-09-08 11:42:50 +0530

    Bug fix for user api

--- controllers/userController.js
+++ controllers/userController.js
@@ -54,29 +54,31 @@ let userController = {
     
             check("password")
             .isLength({min:3})
-            .withMessage("Password must have atleast 3 characters"),
+            .withMessage("Password must have atleast 3 characters")
         ]
     },
 
     validateUser(req, res, next) {
         const errors = validationResult(req)
 
-        if(errors.length != 0){
+        if(errors.isEmpty()){
             logger.log('info', `Status: ${res.statusCode}: Entered data is valid`)
             next()
-        } else {
-            logger.log('error', `Status: 422: ${errors}`)
-            return res.json(errors)
-        }
+        } else{
+            const extractedErrors = []
+            errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }))
+            logger.log('error', `Status: 422: ${extractedErrors}`)
+            return res.status(422).json({message: extractedErrors})
+        } 
     },
 
     async getUserByEmail(req, res, next){
         let user
         try{
             user = await userModel.find({email:req.body.email})
-        } catch (err) {
+        } catch (error) {
             logger.log('error', `Status: ${res.statusCode}: ${error.message}`)
-            return res.status(500).json({ message:err.message })
+            return res.status(500).json({ message:error.message })
         }
 
         res.user = user
